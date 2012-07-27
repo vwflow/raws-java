f362fab4f320aacd15826e646a23c7a6c4504f52
Bruno.Ketelbueters@gmail.com <Bruno.Ketelbueters@gmail.com@e90e6f9a-0baa-9f09-9a50-ea37fdd237d2>
2012-07-27 14:41:59 +0000

    version 0.3

--- trunk/rawsclient/RawsClient.java
+++ trunk/rawsclient/RawsClient.java
@@ -1,6 +1,17 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2012 rambla.eu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package rawsclient;
 
@@ -10,8 +21,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.logging.*;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.methods.HttpGet;
@@ -45,17 +55,12 @@ public class RawsClient {
     private static final int RAMS = 4;
     private static final int RASE = 5;
 
-    public RawsClient(String username, String password, String cdn, Logger logger) 
+    public RawsClient(String username, String password, String cdn) 
     {
         this.username = username;
         this.password = password;
         this.cdn = cdn;
-        if (logger != null) {
-            RawsClient.LOGGER = logger;
-        }
-        else {
-            RawsClient.LOGGER = Logger.getLogger("RawsClient");
-        }
+        RawsClient.LOGGER = Logger.getLogger("RawsClient");
     }
     
     public void setUsername(String username) {
